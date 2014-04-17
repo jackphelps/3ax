548844125fb9b8252431cfed3e3928d47a204137
jackphelps@gmail.com <jackphelps@gmail.com>
2014-04-17 11:31:20 -0400

    express 4 scaffolding, way fucking clean, I like it

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-var express = require('express'),
-    app = express(),
-    port = 3000;
+var express = require('express');
+var routes = require('./server/routes');
+var http = require('http');
+var path = require('path');
 
-app.get('/', function(req, res){
-    res.sendfile('client/views/index.html');
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'client/public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
- 
-app.listen(port);
-console.log('Listening on port ' + port);
