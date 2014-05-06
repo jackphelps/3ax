f6a965b75fb2785abc11e4378ea8018708fefd39
jackphelps@gmail.com <jackphelps@gmail.com>
2014-05-06 09:38:39 -0400

    tests

--- spec/helpers-spec.js
+++ spec/helpers-spec.js
@@ -1,3 +1,5 @@
+// jasmine-node --verbose --color spec --autotest --watch ./lib 
+
 var redisClient = require('../lib/redisclient');
 var helpers = require('../lib/helpers');
 var models = require('../lib/models');
@@ -5,44 +7,39 @@ var models = require('../lib/models');
 describe("generateRandomString", function() {
 
   it("returns a random string of specified length", function() {
-    var res = helpers.generateRandomString(4);
-    expect(res.length).toBe(4);
+    var res = helpers.generateRandomString(7);
+    expect(res.length).toBe(7);
   });
 
 });
 
-//use apiKey class because it includes functions in the constructor
+//test with apiKey class because it includes functions in the constructor
 describe("asyncSave", function() {
 
-  it("saves an object to redis WITH optional callback", function() {
+  it("saves an object to redis WITH optional callback", function(done) {
     var key = new models.ApiKey('thisKeyIsForTestsOnly');
     helpers.asyncSave(key, function(e,r) {
       expect(e).toBe(null);
-      expect(r).toBe(1);
+      //returns 1 if new insertion 0 if overwrite, so can't expect either
+      //expect(r).toBe(1);
+      done();
     });
-  });
-
-  it("sends an object to redis WITHOUT optional callback", function() {
-    var watcher = new models.ApiKey('thisKeyIsForTestsOnly2');
-    helpers.asyncSave(watcher);
   });
 
 });
 
 describe("asyncLoad", function() {
 
-  it("loads an object from redis", function() {
+  it("loads an object from redis", function(done) {
     var key = new models.ApiKey('thisKeyIsForTestsOnly3');
-
     helpers.asyncSave(key, function(e,r) {
       helpers.asyncLoad('ApiKey', key.id, function(e,r) {
         expect(e).toBe(null);
-        expect(JSON.parse(r).id).toBe(key.id);
+        expect(r.id).toBe(key.id);
+        done();
       });
     });
-
   });
 
 });
-
 
